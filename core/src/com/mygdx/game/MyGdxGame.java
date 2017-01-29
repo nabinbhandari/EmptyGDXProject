b8135c19d72486fa2aa2138ce32d10fadaf2fd93
Nabin Bhandari <bnabin51@gmail.com>
2017-01-29 13:06:29 +0545

    add a Box2D object

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -3,31 +3,73 @@ package com.mygdx.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch batch;
+    Sprite background;
+    World world;
+    Box2DDebugRenderer b2dr;
+    Body body;
+    OrthographicCamera camera;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    float screenWidth, screenHeight;
+
+    @Override
+    public void create() {
+        screenWidth = Gdx.graphics.getWidth();
+        screenHeight = Gdx.graphics.getHeight();
+
+        batch = new SpriteBatch();
+        background = new Sprite(new Texture("background.png"));
+        world = new World(new Vector2(0, 10), true);
+        b2dr = new Box2DDebugRenderer();
+        camera = new OrthographicCamera(500f, 500f * screenHeight / screenWidth);
+        camera.position.set(250, 250 * screenHeight / screenWidth, 0);
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+
+        BodyDef bdef = new BodyDef();
+        PolygonShape shape = new PolygonShape();
+        FixtureDef fdef = new FixtureDef();
+
+        Rectangle rect = new Rectangle(0, 0, background.getWidth(), 50);
+
+        bdef.type = BodyDef.BodyType.StaticBody;
+        bdef.position.set(rect.getX() + rect.getWidth() / 2, rect.getY() + rect.getHeight() / 2);
+
+        body = world.createBody(bdef);
+
+        shape.setAsBox(rect.getWidth() / 2, rect.getHeight() / 2);
+        fdef.shape = shape;
+        body.createFixture(fdef);
+    }
+
+    @Override
+    public void render() {
+        // Sets the clear screen color to: Cornflower Blue
+        Gdx.gl.glClearColor(0x64 / 255.0f, 0x95 / 255.0f, 0xed / 255.0f, 0xff / 255.0f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        background.draw(batch);
+        batch.end();
+        b2dr.render(world, camera.combined);
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        background.getTexture().dispose();
+    }
 }
